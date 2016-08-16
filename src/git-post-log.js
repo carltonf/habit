441b2d6020ef4c5eadad7b4e9c77bf1dcae9eb0a
Carl Xiong <xiongc05@gmail.com>
2016-08-16 16:33:22 +0800

    add raw_status to post-log
    * Strip commmit headers for easier parsing

--- src/git-post-log.js
+++ src/git-post-log.js
@@ -5,15 +5,23 @@
 //
 const spawn_sync = require('child_process').spawnSync;
 
-// NOTE `num`: number of log entries to retrieve, 0 or undefined means all
-function git_post_log(post_path, num) {
-  let git_args = ['log', '--', post_path];
+// NOTE `num`: number of log entries to retrieve, 0 or undefined means all.
+// `format`: the format supported by `git log` command, see the man page for
+// details.
+function git_post_log(post_path, num, format) {
+  let git_args = ['log'];
   // NOTE: only pass log entry count when the number arg is given
   // PS: passing empty string '' to spawn_sync yields error
   if (num && typeof num === 'number') {
-    git_args.splice(1, 0, `-${num}`);
+    git_args.push(`-${num}`);
   }
 
+  if (format && typeof format === 'string') {
+    git_args.push(`--format=${format}`);
+  }
+
+  // NOTE finally set target
+  git_args.push('--', post_path);
   let spawn_result = spawn_sync('git', git_args);
   if (spawn_result.status !== 0) {
     throw new Error('* git log failed with status ' + spawn_result.status);
@@ -22,12 +30,18 @@ function git_post_log(post_path, num) {
   return spawn_result.stdout.toString();
 }
 
-// NOTE for convenience
+// NOTE for convenience, show the most recent log
 function git_post_status(post_path) {
   return git_post_log(post_path, 1);
+}
+
+// NOTE for parsing by git-msg-parser
+function git_post_raw_status (post_path) {
+  return git_post_log(post_path, 1, '%s%n%b');
 }
 
 module.exports = {
   log: git_post_log,
   status: git_post_status,
+  raw_status: git_post_raw_status,
 }
