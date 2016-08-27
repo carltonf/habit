6493eb9776c2559156788b91ea542fcd5d9dbc8d
Carl Xiong <xiongc05@gmail.com>
2016-08-27 13:23:45 +0800

    add log subcommand
    * To display all log associated with a post
    
    * This subcommand also demos the effectiveness of `--follow` option to
      `git log`

---
+++ src/subcmd/log.js
@@ -0,0 +1,44 @@
+const helpers = require('./_helpers');
+
+function registerTo(program) {
+  program
+    .command('log [post_path]')
+    .description('Show the complete Git log for `post_path` or `cwp`.')
+    .action(function (post_path) {
+      post_path = post_path || helpers.getCWP();
+
+      try {
+        let action = new LogAction(post_path);
+        let log = action.getLog();
+
+        console.log( log );
+      } catch (e) {
+        console.error(e.message);
+
+        return -1;
+      }
+    });
+}
+
+module.exports = {
+  registerTo: registerTo,
+}
+
+//////////////////////////
+
+function LogAction(postPath) {
+  this.postPath = postPath;
+
+  helpers.assertPostPath(postPath);
+}
+
+LogAction.prototype.getLog = function getLog () {
+  return this.__getLog();
+}
+
+// External Deps
+const post_logger = require('../git-post-log');
+
+LogAction.prototype.__getLog = function __getLog () {
+  return post_logger.log(this.postPath);
+}
